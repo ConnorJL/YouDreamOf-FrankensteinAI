de92f3086c37c9614bd47f478d00bd814d510bc5
connorjl <cjleahy95@googlemail.com>
2018-12-01 22:33:21 +0100

    Test netcode

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from create_dream import create_dream
 app = Flask(__name__, static_url_path='')
 
 @app.route('/create')
-def create_dream(path):
+def create_dream():
     dream =request.args.get("dreamKeyword")
     numNPCTexts = request.args.get("numNPCTexts")
     numObjects = request.args.get("numObjects")
@@ -13,6 +13,10 @@ def create_dream(path):
     data = {"dreamKeyword":dream, "numNPCTexts": numNPCTexts, "numObjects": numObjects, "numPeople" : numPeople}
     create_dream(data)
     return send_from_directory(dream + ".zip")
+
+@app.route('/')
+def test():
+    return send_from_directory("test.zip")
 
 if __name__ == "__main__":
     app.run()
